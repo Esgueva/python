405dcb6597324de1bdc11f164d87646599b7f42e
Rodrigo Esgueva <esgueva@gmail.com>
2019-07-31 20:28:10 +0200

    CUDSLE vRAM

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 
 clients = 'Rodrigo, Julia, Mario, '
 
@@ -33,6 +34,21 @@ def delete_client(client_name):
     pass
 
 
+def search_client(client_name):
+    """ Search client name in client list """
+    global clients
+    clients_list = clients.split(',')
+    
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            print('The client {} is in the client\'s list'.format(client_name)) 
+            return True
+
+    print('The client {} not in the client\'s list'.format(client_name))
+
+            
 def list_clients():
     """ List client """
     global clients
@@ -50,44 +66,83 @@ def _client_not_found():
     return print('Client is not in clients list')
 
 
-def _print_welcome():
-    """ Wellcome to aplication """
-    print('*' * 35)
-    print('*    Welcome to Esgueva Company   *')
-    print('*' * 35)
-    print('| What wolud you like to do today? |')
-    print('-' * 34)
-    print('[C]reate client')
-    print('[U]pdate client')
-    print('[D]elete client')
-    print('[L]ist   clients')
-
-
 def _get_client_name():
     """ Return client name """
-    return input('What is the client name?')
-
-
-if __name__ == '__main__':
-    """ Main App """
     
-    _print_welcome()
-    
-    command = input()
-    command = command.upper()
+    client_name = None
 
+    while not client_name:
+        client_name = input('What is the client name? ')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+        
+    if not client_name:
+        sys.exit()
+
+    return client_name
+
+
+def _get_command():
+    """ Ask command """
+
+    command = None
+    
+    while not command:
+        command = input('Please enter the command: ')
+    
+    return command.upper()
+
+
+def _print_welcome():
+    """ Wellcome to aplication """
+    print('')
+    print('')
+    print('*' * 36)
+    print('*    Welcome to Esgueva Company    *')
+
+
+def _print_options():
+    print('*' * 36)
+    print('| What wolud you like to do today? |')
+    print('-' * 36)
+    print('|         [C]reate client          |')
+    print('|         [U]pdate client          |')
+    print('|         [D]elete client          |')
+    print('|         [S]earch client          |')
+    print('|         [L]ist   clients         |')
+    print('|         [E]xit   program         |')
+    print('-' * 36)
+    print('')
+
+
+def setup():
+    command = _get_command()
     if command == 'C':
         create_client(_get_client_name())
         list_clients()
     elif command == 'U':
-        client_name = _get_client_name()
+        old_client_name = _get_client_name()
         new_client_name = input('What is the updated client name?')
-        update_client(client_name,new_client_name)
+        update_client(old_client_name,new_client_name)
         list_clients()
     elif command == 'D':
         delete_client(_get_client_name())
         list_clients()
     elif command == 'L':
         list_clients()
+    elif command == 'S':
+        search_client(_get_client_name())
+    elif command == 'E':
+        sys.exit()
     else:
         print('Invalid command')
+
+if __name__ == '__main__':
+    """ Main App """
+    
+    _print_welcome()
+    _print_options()
+    setup()
+    
